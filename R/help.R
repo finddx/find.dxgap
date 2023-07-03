bdc76c779069561af4f64ffff109a8c0eb6ac399
Andrea Ranzato <andrea@cynkra.com>
2023-07-03 15:59:00 +0200

    Helpers

--- R/help.R
+++ R/help.R
@@ -9,3 +9,13 @@ compose_file_path <- function(file_name, data_dir) {
     file.path(data_dir, file_name)
   }
 }
+read_tidy_tb <- function(type = "time_series") {
+  suppressMessages(read_tb_dashboard()) |>
+    tidy_tb_dashboard(type = type)
+}
+
+get_distinct <- function(col, df = read_tidy_tb()) {
+  df |>
+    dplyr::distinct({{ col }}) |>
+    dplyr::pull()
+}
