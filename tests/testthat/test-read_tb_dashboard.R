298aac45311c5821dfd93859b0c18b758152f3c4
Andrea Ranzato <andrea@cynkra.com>
2023-07-03 21:01:09 +0200

    Tweak read and tidy dashboard tests

--- tests/testthat/test-read_tb_dashboard.R
+++ tests/testthat/test-read_tb_dashboard.R
@@ -1,8 +1,14 @@
+file_name <- "tb_policy_dashboard_final_v1_14Nov22.xlsx"
+read_tb_dashboard_mem <- memoise::memoise(read_tb_dashboard)
 test_that("a tibble is returned", {
-  expect_true(tibble::is_tibble(read_tb_dashboard()))
+  skip_if_no_file(file_name)
+  data <- read_tb_dashboard_mem(file_name)
+  expect_true(tibble::is_tibble(data))
 })
 
 test_that("the tibble dimensions are as expected", {
-  expect_equal(nrow(read_tb_dashboard()), 44)
-  expect_equal(ncol(read_tb_dashboard()), 89)
+  skip_if_no_file(file_name)
+  data <- read_tb_dashboard_mem(file_name)
+  expect_equal(nrow(data), 44)
+  expect_equal(ncol(data), 89)
 })
