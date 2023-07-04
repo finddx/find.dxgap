b350995d5a1c4c90860a9d7b4e35b54f4ee8054d
Andrea Ranzato <andrea@cynkra.com>
2023-07-04 09:58:31 +0200

    No mem

--- tests/testthat/test-tidy_tb_dashabord.R
+++ tests/testthat/test-tidy_tb_dashabord.R
@@ -1,15 +1,13 @@
 file_name <- "tb_policy_dashboard_final_v1_14Nov22.xlsx"
-read_tb_dashboard_mem <- memoise::memoise(read_tb_dashboard)
+data <- read_tb_dashboard(file_name)
 test_that("a tibble is returned", {
   skip_if_no_file(file_name)
-  data <- read_tb_dashboard_mem(file_name)
   expect_true(tibble::is_tibble(tidy_tb_dashboard(data)))
   expect_true(tibble::is_tibble(tidy_tb_dashboard(data, type = "fixed")))
 })
 
 test_that("the tibble dimensions are as expected", {
   skip_if_no_file(file_name)
-  data <- read_tb_dashboard_mem(file_name)
   expect_equal(nrow(tidy_tb_dashboard(data, type = "time_series")), 1440)
   expect_equal(ncol(tidy_tb_dashboard(data, type = "time_series")), 4)
   expect_equal(nrow(tidy_tb_dashboard(data, type = "fixed")), 30)
@@ -18,7 +16,6 @@ test_that("the tibble dimensions are as expected", {
 
 test_that("primary keys match across time series and fixed data", {
   skip_if_no_file(file_name)
-  data <- read_tb_dashboard_mem(file_name)
   expect_equal(
     unique(tidy_tb_dashboard(data, type = "time_series")$country),
     unique(tidy_tb_dashboard(data, type = "fixed")$country)
