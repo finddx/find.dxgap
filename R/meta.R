7fb0bd7c331fda375e9feab2d700d6dfe0df033d
Andrea Ranzato <andrea@cynkra.com>
2023-07-27 15:34:10 +0200

    Url endpoints conversion

---
+++ R/meta.R
@@ -0,0 +1,9 @@
+who_url_endpoints <- tibble::tribble(
+                     ~dataset,                   ~url_endpoint,
+                       "budget",                  "budget",
+         "community_engagement",               "community",
+                    "estimates",               "estimates",
+  "expenditure_and_utilisation", "expenditure_utilisation",
+                 "laboratories",                    "labs",
+                 "notification",           "notifications"
+  )
