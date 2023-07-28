d764c462490bea96d89a9d1b8358086ebbfc9d96
Andrea Ranzato <andrea@cynkra.com>
2023-07-28 09:37:22 +0200

    Add pytpe budget

--- R/meta.R
+++ R/meta.R
@@ -15,3 +15,19 @@ ptype_who_notifications <- tibble::tibble(
   new_labconf = numeric(0),
   download_date = character(0),
 )
+
+ptype_who_budget <- tibble::tibble(
+  country = character(0),
+  year = numeric(0),
+  budget_lab = numeric(0),
+  budget_oth = numeric(0),
+  budget_staff = numeric(0),
+  cf_lab = numeric(0),
+  cf_staff = numeric(0),
+  cf_tot_domestic = numeric(0),
+  cf_tot_gf = numeric(0),
+  cf_tot_grnt = numeric(0),
+  cf_tot_sources = numeric(0),
+  cf_tot_usaid = numeric(0),
+  download_date = character(0),
+)
