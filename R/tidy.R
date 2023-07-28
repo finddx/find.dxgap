feaab860ef8edc7a8e4aa99fc5e4b425e1ebd8fd
Andrea Ranzato <andrea@cynkra.com>
2023-07-28 09:37:53 +0200

    Use ptype

--- R/tidy.R
+++ R/tidy.R
@@ -246,13 +246,15 @@ tidy_who <- function(data) {
   cond_notifications <- is_ptype(data, ptype_who_notifications)
   # TODO:
   # cond_community <- is_ptype(data, ptype_who_community)
-  # cond_budget <- is_ptype(data, ptype_who_budget)
+  cond_budget <- is_ptype(data, ptype_who_budget)
   # cond_estimates <- is_ptype(data, ptype_who_estimates)
   # cond_expenditure <- is_ptype(data, ptype_who_expenditure)
   # cond_labs <- is_ptype(data, ptype_who_labs)
 
   if (cond_notifications) {
     tidy_who_notifications(data)
+  } else if (cond_budget) {
+    tidy_who_budget(data)
   } else {
     rlang::abort("Unknown ptype.")
   }
