2961d57fbd9dc369d9c4a8ef99c0be2abcf2b179
Andrea Ranzato <andrea@cynkra.com>
2023-08-30 16:00:45 +0200

    Move xpert

--- R/meta.R
+++ R/meta.R
@@ -75,10 +75,10 @@ ptype_who_labs <- tibble::tibble(
   lab_sm = numeric(0),
   smear = numeric(0),
   lab_xpert = numeric(0),
+  xpert = numeric(0),
   m_wrd = numeric(0),
   m_wrd_tests_performed = numeric(0),
   m_wrd_tests_positive = numeric(0),
-  xpert = numeric(0),
   download_date = character(0),
 )
 
