3ac848d4b2d1ea92ea134982b8020a8fcffe42b1
Andrea Ranzato <andrea@cynkra.com>
2023-08-31 09:59:56 +0200

    Rework WB test

--- tests/testthat/test-wb.R
+++ tests/testthat/test-wb.R
@@ -1,14 +1,17 @@
-test_that("World Bank data is downloaded, read and tidied correctly", {
-  skip_if(Sys.getenv("FINDTB_DATADIR") == "")
-  skip_if_offline()
-  skip_if_not_available("https://api.worldbank.org/v2/country/all/indicator")
-  path <- download_wb(range_years = "2021:2022")
+skip_if(Sys.getenv("FINDTB_DATADIR") == "")
+skip_if_offline()
+skip_if_not_available("https://api.worldbank.org/v2/country/all/indicator")
+path <- download_wb(range_years = "2021:2022")
+on.exit(file.remove(path))
+raw <- read_wb_pop_density(path)
+tidy <- tidy_wb_pop_density(raw)
+test_that("World Bank data is downloaded correctly", {
   expect_true(file.exists(path))
-  on.exit(file.remove(path))
+})
+
+test_that("World Bank data is read and tidied correctly", {
   expect_snapshot({
-    raw <- read_wb_pop_density(path)
     constructive::construct(vctrs::vec_ptype(raw))
-    tidy <- tidy_wb_pop_density(raw)
     constructive::construct(vctrs::vec_ptype(tidy))
   })
 })
