936273ee023accf19e682e2ddcf0f1a672670048
Andrea Ranzato <andrea@cynkra.com>
2023-08-31 12:02:58 +0200

    Add test for pop density

---
+++ tests/testthat/test-wb_pop_density.R
@@ -0,0 +1,25 @@
+skip_if(Sys.getenv("FINDTB_DATADIR") == "")
+skip_if_offline()
+skip_if_not_available("https://api.worldbank.org/v2/country/all/indicator")
+path <- download_wb(indicator = "EN.POP.DNST", range_years = "2021:2022")
+on.exit(file.remove(path))
+raw <- read_wb(path)
+tidy <- tidy_wb(raw)
+test_that("World Bank data is downloaded correctly", {
+  expect_true(file.exists(path))
+})
+
+test_that("World Bank data is read and tidied correctly", {
+  expect_snapshot({
+    constructive::construct(vctrs::vec_ptype(raw))
+    constructive::construct(vctrs::vec_ptype(tidy))
+  })
+})
+
+test_that("`country_code` entries are not missing", {
+  expect_snapshot(
+    tidy |>
+      dplyr::distinct(country_code, country_value) |>
+      dplyr::filter(is.na(country_code))
+  )
+})
