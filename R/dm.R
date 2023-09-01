7b4d3bcab57e6ccaa05a15bc2545642d8a3faa6f
Andrea Ranzato <andrea@cynkra.com>
2023-09-01 16:05:57 +0200

    Build dm init

---
+++ R/dm.R
@@ -0,0 +1,25 @@
+findtb_build_dm <- function(data_list) {
+  dm_no_rel <- dm::dm(!!!data_list)
+  dm_rel <- set_dm_rels(dm_no_rel)
+  dm_rel
+}
+
+set_dm_rels <- function(dm) {
+  dm |>
+    dm::dm_add_pk(hbc_df, country_code) |>
+    dm::dm_add_pk(wb_tot_pop_df, country_code) |>
+    dm::dm_add_pk(wb_urb_pop_df, country_code) |>
+    dm::dm_add_pk(wb_density_pop_df, country_code) |>
+    dm::dm_add_pk(who_notifications_df, country_code) |>
+    dm::dm_add_pk(who_estimates_df, country_code) |>
+    dm::dm_add_pk(gf_procurement_df, country_code) |>
+    dm::dm_add_pk(who_budget_df, c(country_code, budget_type)) |>
+    dm::dm_add_pk(who_community_df, c(country_code, comm_type)) |>
+    dm::dm_add_pk(who_sites_df, c(country_code, site_type)) |>
+    dm::dm_add_fk(hbc_df, country_code, wb_tot_pop_df) |>
+    dm::dm_add_fk(hbc_df, country_code, wb_urb_pop_df) |>
+    dm::dm_add_fk(hbc_df, country_code, wb_density_pop_df) |>
+    dm::dm_add_fk(hbc_df, country_code, who_notifications_df) |>
+    dm::dm_add_fk(hbc_df, country_code, who_estimates_df) |>
+    dm::dm_add_fk(hbc_df, country_code, gf_procurement_df)
+}
