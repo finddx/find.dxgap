7b51456470dc340fad296b7bd5eec60403fbb9ce
Andrea Ranzato <andrea@cynkra.com>
2023-09-01 17:02:24 +0200

    Drop

--- R/dm.R
+++ R/dm.R
@@ -1,5 +1,9 @@
 findtb_build_dm <- function(data_list) {
-  dm_no_rel <- dm::dm(!!!data_list)
+  cols_to_drop <- c("year", "country", "g_whoregion")
+  prune_lst <-
+    data_list |>
+    purrr::map(~ dplyr::select(.x, -tidyselect::any_of(cols_to_drop)))
+  dm_no_rel <- dm::dm(!!!prune_lst)
   dm_rel <- set_dm_rels(dm_no_rel)
   dm_rel
 }
