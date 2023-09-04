8e7fc8a66340e3e6ba79f5eb27ad97df0d49b784
Andrea Ranzato <andrea@cynkra.com>
2023-09-04 16:01:07 +0200

    Add get_hbc_core()

--- R/hbc.R
+++ R/hbc.R
@@ -38,3 +38,23 @@ tidy_hbc <- function(data, year = NULL) {
   }
   df
 }
+
+get_hbc_core <- function(data) {
+  years_unique <-
+    data |>
+    dplyr::distinct(year) |>
+    dplyr::pull(year)
+
+  if (length(years_unique) <= 1) {
+    rlang::abort(
+      c("Cannot build `core high TB burden country` data across years.",
+      i = "Did you call `tidy_hbc(data, year = NULL)`?.")
+    )
+  }
+
+  data |>
+    dplyr::select(country_code, year) |>
+    dplyr::group_split(year) |>
+    purrr::reduce(dplyr::inner_join, by = dplyr::join_by(country_code)) |>
+    dplyr::select(-tidyselect::starts_with("year"))
+}
