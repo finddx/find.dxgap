6a88f139c1c25f4c47c69335a9d02a64b2f12c8b
Andrea Ranzato <andrea@cynkra.com>
2023-09-04 23:36:10 +0200

    Import script

---
+++ inst/scripts/import_script.R
@@ -0,0 +1,14 @@
+pkgload::load_all()
+
+lst <- findtb_import(lst, "gf_2023-07-26_procurement.csv", 2019)
+lst <- findtb_import(lst, "wb_2023-07-28_pop_urban.csv", 2019)
+lst <- findtb_import(lst, "wb_2023-08-31_pop_density.csv", 2019)
+lst <- findtb_import(lst, "wb_2023-08-31_pop_total.csv", 2019)
+lst <- findtb_import(lst, "who_2023-07-28_budget.csv", 2019)
+lst <- findtb_import(lst, "who_2023-07-28_community.csv", 2019)
+lst <- findtb_import(lst, "who_2023-07-28_estimates.csv", 2019)
+lst <- findtb_import(lst, "who_2023-07-28_expenditures.csv", 2019)
+lst <- findtb_import(lst, "who_2023-07-28_notifications.csv", 2019)
+lst <- findtb_import(lst, "who_2023-08-30_laboratories.csv", 2019)
+lst <- findtb_import(lst, "who_hbc.csv", 2019)
+
