47ace1bd6ce323758505987c103719a3ab526339
Andrea Ranzato <andrea@cynkra.com>
2023-09-04 21:11:45 +0200

    Import init

---
+++ R/import.R
@@ -0,0 +1,73 @@
+findtb_import <- function(.file_name,
+                          .year,
+                          .all = TRUE,
+                          .data_dir = Sys.getenv("FINDTB_DATADIR")) {
+
+  # Extract file name ----------------------------------------------------------
+  data_name <- extract_name(.file_name)
+
+  data_summary <-
+    data_summary |>
+    dplyr::mutate(data_name = stringr::str_c(provider, dataset, sep = "_")) |>
+    dplyr::filter(data_name == !!data_name)
+
+  provider <-
+    data_summary |>
+    dplyr::pull(provider)
+
+  dataset <-
+    data_summary |>
+    dplyr::pull(dataset)
+
+  # Read -----------------------------------------------------------------------
+
+  path_file <- compose_file_path(file_name = .file_name, data_dir = .data_dir)
+
+  read_raw <- switch(
+    data_name,
+    who_budget = read_who,
+    who_community = read_who,
+    who_estimates = read_who,
+    who_expenditures = read_who,
+    who_laboratories = read_who,
+    who_notifications = read_who,
+    who_hbc = read_hbc,
+    gf_procurement = read_gf_procurement,
+    wb_pop_total = read_wb,
+    wb_pop_density = read_wb,
+    wb_pop_urban = read_wb
+  )
+
+  data_raw <- read_raw(path_file)
+
+  # Tidy -----------------------------------------------------------------------
+
+  tidy_data <- switch(
+    data_name,
+    who_budget = tidy_who,
+    who_community = tidy_who,
+    who_estimates = tidy_who,
+    who_expenditures = tidy_who,
+    who_laboratories = tidy_who,
+    who_notifications = tidy_who,
+    who_hbc = tidy_hbc,
+    gf_procurement = tidy_gf_procurement,
+    wb_pop_total = tidy_wb,
+    wb_pop_density = tidy_wb,
+    wb_pop_urban = tidy_wb
+  )
+
+  if (provider %in% c("who") && dataset != "hbc") {
+    data_tidy <- tidy_data(data_raw, year = .year, shape = "wide")
+  } else {
+    data_tidy <- tidy_data(data_raw, year = .year)
+  }
+
+  data_tidy
+
+}
+
+
+extract_name <- function(file_name) {
+  stringr::str_remove_all(file_name, "\\d{4}-\\d{2}-\\d{2}\\_|\\.csv$") # TODO ext should not be hardcoded
+}
