e74bbb54209356c909b782858f727cdf68694fef
Andrea Ranzato <andrea@cynkra.com>
2023-09-04 12:27:25 +0200

    Draw

--- inst/scripts/tbl.R
+++ inst/scripts/tbl.R
@@ -2,6 +2,9 @@ library(tidyverse)
 pkgload::load_all()
 df_lst <- findtb_load(.year = 2021)
 dm <- findtb_build_dm(df_lst)
+
+dm::dm_draw(dm)
+
 tbl <-
   dm |>
   dm::dm_flatten_to_tbl(.start = hbc)
