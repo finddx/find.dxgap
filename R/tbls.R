0c3c6da9b7b763a851b32c680e353441770c5f26
Andrea Ranzato <andrea@cynkra.com>
2023-09-04 17:46:37 +0200

    Add data summary

---
+++ R/tbls.R
@@ -0,0 +1,15 @@
+data_summary <-
+  tibble::tribble(
+    ~data_source,                      ~dataset,
+    "who",                             "budget",
+    "who",                          "community",
+    "who",                          "estimates",
+    "who",                       "expenditures",
+    "who",                       "laboratories",
+    "who",                       "notifications",
+    "who",                                "hbc",
+    "gf",                         "procurement",
+    "wb",                            "pop_total",
+    "wb",                         "pop_density",
+    "wb",                          "pop_urban"
+  )
