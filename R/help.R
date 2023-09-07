ea8e8f24a67c66eb3ba03a69f74158c0d7c98baa
Andrea Ranzato <andrea@cynkra.com>
2023-09-07 15:06:47 +0200

    Move

--- R/help.R
+++ R/help.R
@@ -30,3 +30,37 @@ skip_if_not_available <- function(url) {
     testthat::skip("Cannot reach online resource.")
   }
 }
+
+skip_if_no_data <- function(file_name,
+                            data_dir = Sys.getenv("FINDTB_DATADIR")) {
+  skip_if(data_dir == "")
+  path <- file.path(data_dir, file_name)
+  skip_if(!file.exists(path))
+}
+
+compose_date_dataset_file_name <- function(...,
+                                           download_date = as.character(Sys.Date()),
+                                           dataset = "notifications") {
+  compose_file_name(..., download_date, dataset)
+}
+
+compose_file_name <- function(..., sep = "_", file_ext = NULL) {
+  dots <- rlang::dots_list(...)
+  if (is.null(file_ext)) {
+    file_name <- paste(dots, collapse = sep)
+    return(file_name)
+  }
+  paste0(paste(dots, collapse = sep), file_ext)
+}
+
+findtb_read_csv <- function(file_name, data_dir = Sys.getenv("FINDTB_DATADIR"), ...) {
+  file_path <- compose_file_path(file_name = file_name, data_dir = data_dir)
+  readr::read_csv(file_path, show_col_types = FALSE, ...)
+}
+
+req_perform_lazy <- function(req) {
+  stopifnot(inherits(req, "httr2_request"))
+  Sys.sleep(3)
+  httr2::req_perform(req = req)
+}
+
