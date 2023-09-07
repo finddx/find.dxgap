c8c01e19dfe97a9c24dc955ee726e4892dcc1577
Andrea Ranzato <andrea@cynkra.com>
2023-09-07 19:23:43 +0200

    Tweaks

--- inst/scripts/lm.R
+++ inst/scripts/lm.R
@@ -1,44 +1,53 @@
+# library(tidyverse)
 pkgload::load_all()
 
 a_lst <- findtb_load()
-
 dm <- findtb_build_dm(a_lst)
 
 tbl <-
   dm::dm_flatten_to_tbl(dm, .start = hbc) |>
-  compute_dx_gap()
+  dplyr::mutate(
+    who_dx_gap = (e_inc_num - c_newinc) / e_inc_num,
+    pop_100k = pop_total / 1e5
+  ) |>
+  dplyr::select(-country_code)
+
+to_divide_by_pop <- c(
+  "budget_lab",
+  "budget_staff",
+  "cf_tot_domestic",
+  "cf_tot_sources",
+  "culture",
+  "smear",
+  "xpert",
+  "total_numb_device"
+  )
 
 tbl_rec <-
   recipes::recipe(who_dx_gap ~ ., data = tbl) |>
-  recipes::update_role(country_code, new_role = "id") |>
+  # recipes::update_role(country_code, new_role = "id") |>
   recipes::update_role(who_dx_gap, new_role = "outcome") |>
+  recipes::update_role(e_inc_100k, new_role = "impute") |>
+  recipes::update_role(pop_total, new_role = "impute") |>
   recipes::step_mutate(
-    pop_100k = pop_total / 1e5,
-    budget_lab_100k = budget_lab / pop_100k,
-    budget_staff_100k = budget_staff / pop_100k,
-    cf_tot_domestic_100k = cf_tot_domestic / pop_100k,
-    cf_tot_sources_100k = cf_tot_sources / pop_100k,
-    culture_100k = culture / pop_100k,
-    smear_100k = smear / pop_100k,
-    xpert_100k = xpert / pop_100k,
-    total_dev_100k = total_numb_device / pop_100k
+    dplyr::across(
+      dplyr::all_of(to_divide_by_pop),
+      ~ .x / pop_100k,
+      .names = "{.col}_100k"
+    )
   ) |>
-  # recipes::step_select(
-  #   who_dx_gap,
-  #   pop_100k,
-  #   budget_lab_100k,
-  #   budget_staff_100k,
-  #   cf_tot_domestic_100k,
-  #   cf_tot_sources_100k,
-  #   culture_100k,
-  #   smear_100k,
-  #   xpert_100k,
-  #   total_dev_100k
-  # ) |>
-  recipes::step_rm(country_code) |>
-  recipes::step_normalize(recipes::all_numeric()) |>
-  recipes::step_corr(-recipes::all_outcomes()) |>
-  recipes::step_impute_knn(-recipes::all_outcomes(), neighbors = 3)
+  recipes::step_rm(c_cdr, e_mort_100k, c_newinc_100k) |>
+  recipes::step_zv(-recipes::all_outcomes()) |>
+  recipes::step_normalize(-recipes::all_outcomes()) |>
+  recipes::step_impute_knn(
+    -recipes::all_outcomes(),
+    neighbors = 3,
+    impute_with = recipes::has_role("impute")
+  ) |>
+  recipes::step_corr(-recipes::all_outcomes(), threshold = 0.8)
+
+recipes::prep(tbl_rec) |>
+  recipes::bake(tbl)
 
 lm_model <-
   parsnip::linear_reg(mode = "regression") %>%
