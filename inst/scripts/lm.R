f304d457c22d012099ae00711f0cb13361eecd62
Andrea Ranzato <andrea@cynkra.com>
2023-09-07 20:50:55 +0200

    Tweak

--- inst/scripts/lm.R
+++ inst/scripts/lm.R
@@ -13,6 +13,7 @@ tbl <-
   dplyr::select(-country_code)
 
 to_divide_by_pop <- c(
+  "gdp",
   "budget_lab",
   "budget_staff",
   "cf_tot_domestic",
@@ -25,29 +26,53 @@ to_divide_by_pop <- c(
 
 tbl_rec <-
   recipes::recipe(who_dx_gap ~ ., data = tbl) |>
+  recipes::step_rm(
+    c_cdr,
+    e_mort_100k,
+    c_newinc_100k,
+    dplyr::starts_with("lab"),
+    dplyr::starts_with("m_wrd"),
+  ) |>
+  # recipes::step_filter_missing(threshold = 0.75) |>
   # recipes::update_role(country_code, new_role = "id") |>
   recipes::update_role(who_dx_gap, new_role = "outcome") |>
   recipes::update_role(e_inc_100k, new_role = "impute") |>
   recipes::update_role(pop_total, new_role = "impute") |>
+  recipes::update_role(gdp, new_role = "impute") |>
+  recipes::step_impute_median(
+    e_inc_100k,
+    budget_oth,
+    gdp,
+    cf_tot_gf,
+    cf_tot_grnt
+  ) |>
+  recipes::step_impute_knn(
+    -recipes::all_outcomes(),
+    neighbors = 3,
+    impute_with = c("e_inc_100k", "pop_total", "gdp") #recipes::has_role("impute")
+  ) |>
   recipes::step_mutate(
     dplyr::across(
-      dplyr::all_of(to_divide_by_pop),
+      -dplyr::all_of(c("who_dx_gap", "pop_100k", "pop_density", "pop_urban_perc")),
       ~ .x / pop_100k,
       .names = "{.col}_100k"
     )
   ) |>
-  recipes::step_rm(c_cdr, e_mort_100k, c_newinc_100k) |>
+  recipes::step_mutate(
+    dplyr::across(
+      -recipes::all_outcomes(),
+      ~ dplyr::if_else(.x == 0, 10, .x)
+    )
+  ) |>
+  recipes::step_log(-recipes::all_outcomes()) |>
+  recipes::step_lincomb(-recipes::all_outcomes()) |>
   recipes::step_zv(-recipes::all_outcomes()) |>
   recipes::step_normalize(-recipes::all_outcomes()) |>
-  recipes::step_impute_knn(
-    -recipes::all_outcomes(),
-    neighbors = 3,
-    impute_with = recipes::has_role("impute")
-  ) |>
   recipes::step_corr(-recipes::all_outcomes(), threshold = 0.8)
 
 recipes::prep(tbl_rec) |>
-  recipes::bake(tbl)
+  recipes::bake(tbl) |>
+  skimr::skim()
 
 lm_model <-
   parsnip::linear_reg(mode = "regression") %>%
