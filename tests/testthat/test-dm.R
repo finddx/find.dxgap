246b72f09f038ac0c35f39c322acd2e3bae99b05
Andrea Ranzato <andrea@cynkra.com>
2023-09-09 18:14:24 +0200

    Add test

--- tests/testthat/test-dm.R
+++ tests/testthat/test-dm.R
@@ -1,6 +1,11 @@
 skip_if(Sys.getenv("FINDTB_DATADIR") == "")
-test_that("findtb_build_dm() works", {
-  data_list <- findtb_load(.year = 2019)
+data_list <- findtb_load(.year = 2019)
+test_that("findtb_build_dm() `hbc` works", {
   dm <- findtb_build_dm(data_list)
+  expect_snapshot(dm::glimpse(dm))
+})
+
+test_that("findtb_build_dm() `non_hbc` works", {
+  dm <- findtb_build_dm(data_list, hbc = FALSE)
   expect_snapshot(dm::glimpse(dm))
 })
