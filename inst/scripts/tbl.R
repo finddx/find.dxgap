f3ed15a5df2777368a56b9207e4880d656d184de
Andrea Ranzato <andrea@cynkra.com>
2023-09-11 14:28:49 +0200

    Example

--- inst/scripts/tbl.R
+++ inst/scripts/tbl.R
@@ -1,11 +1,17 @@
 library(tidyverse)
 pkgload::load_all()
-df_lst <- findtb_load(.year = 2021)
-dm <- findtb_build_dm(df_lst)
+df_lst <- findtb_load(.year = 2019)
+dm_hbc <- findtb_build_dm(df_lst)
+dm_non_hbc <- findtb_build_dm(df_lst, hbc = FALSE)
 
-dm::dm_draw(dm)
+dm::dm_draw(dm_hbc)
+dm::dm_draw(dm_non_hbc)
 
-tbl <-
-  dm |>
+tbl_hbc <-
+  dm_hbc |>
   dm::dm_flatten_to_tbl(.start = hbc)
+
+tbl_non_hbc <-
+  dm_non_hbc |>
+  dm::dm_flatten_to_tbl(.start = non_hbc)
 
