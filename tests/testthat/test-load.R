bb647b37e521472d1181aff34195410665f526ea
Andrea Ranzato <andrea@cynkra.com>
2023-09-15 15:39:23 +0200

    Bump due to recently introduce WB GDP data

--- tests/testthat/test-load.R
+++ tests/testthat/test-load.R
@@ -1,6 +1,6 @@
 test_that("findtb_load() works", {
   lst_df <- findtb_load()
   expect_type(lst_df, "list")
-  expect_equal(length(lst_df), 10)
+  expect_equal(length(lst_df), 11)
   expect_true(all(purrr::map_int(lst_df, nrow) > 0))
 })
