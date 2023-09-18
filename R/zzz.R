6c60a8ca511dd2b4d3755d0c243233e81854a1b4
Andrea Ranzato <andrea@cynkra.com>
2023-09-18 18:10:46 +0200

    zzz

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(lib, pkg) {
   if (Sys.getenv("DXGAP_DATADIR") == "") {
-    path <- file.path(dirname(getwd()), "findtb-data")
+    path <- file.path(dirname(getwd()), "find.dxgap.data")
     Sys.setenv("DXGAP_DATADIR" = as.character(path))
   }
 }
