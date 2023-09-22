4c56fad6f14f326168875230a080047b2c476268
Andrea Ranzato <andrea@cynkra.com>
2023-09-22 15:40:27 +0200

    From

--- R/explain.R
+++ R/explain.R
@@ -2,7 +2,7 @@ get_core_recipe <- function(tbl) {
   recipes::recipe(formula = who_dx_gap ~ ., x = tbl) |>
     recipes::update_role(who_dx_gap, new_role = "outcome") |>
     recipes::update_role(country_code, new_role = "id") |>
-    recipes::step_filter_missing(all_predictors(), threshold = 0.9) |>
+    recipes::step_filter_missing(recipes::all_predictors(), threshold = 0.9) |>
     recipes::update_role(
       dplyr::all_of(
         c("c_cdr", "c_newinc_100k", "notified_ref_community", "notified_ref",
@@ -24,9 +24,9 @@ get_core_recipe <- function(tbl) {
     recipes::step_filter(
       !if_any(c("gdp", "e_inc_num", "who_dx_gap", "pop_total"), is.na)
     ) |>
-    recipes::step_impute_median(has_role("impute_w_median")) |>
+    recipes::step_impute_median(recipes::has_role("impute_w_median")) |>
     recipes::step_mutate(pop_100k = pop_total / 1e5) |>
-    recipes::step_rm(has_role("collinear")) |>
+    recipes::step_rm(recipes::has_role("collinear")) |>
     recipes::update_role(
       dplyr::all_of(c("gdp", "e_inc_num", "pop_total")),
       new_role = "imputer_knn",
@@ -35,8 +35,8 @@ get_core_recipe <- function(tbl) {
     recipes::step_impute_knn(
       all_predictors(),
       neighbors = 5,
-      impute_with = imp_vars(has_role("imputer_knn"))
+      impute_with = recipes::imp_vars(recipes::has_role("imputer_knn"))
     ) |>
     recipes::step_rm(e_inc_num, e_inc_100k, c_newinc) |>
-    recipes::step_zv(all_numeric_predictors())
+    recipes::step_zv(recipes::all_numeric_predictors())
 }
