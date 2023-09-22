a6178e864339c08365a567d01f130b5c8798dfcb
Andrea Ranzato <andrea@cynkra.com>
2023-09-22 15:15:34 +0200

    Move

--- inst/scripts/tbl.R
+++ inst/scripts/tbl.R
@@ -3,13 +3,15 @@ pkgload::load_all()
 df_lst <- load(.year = 2019)
 dm_hbc <- build_dm(df_lst)
 dm_non_hbc <- build_dm(df_lst, hbc = FALSE)
+data_tbl <- build_tbl(dm_hbc, dm_non_hbc)
+
 
 dm::dm_draw(dm_hbc)
 dm::dm_examine_constraints(dm_hbc)
 dm::dm_draw(dm_non_hbc)
 dm::dm_examine_constraints(dm_non_hbc)
 
-data_tbl <- build_tbl(dm_hbc, dm_non_hbc)
+
 
 tbl_hbc <-
   dm_hbc |>
