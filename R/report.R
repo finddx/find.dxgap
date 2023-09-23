c76c98593b6b31c81f44a4195664ea913438d58e
Andrea Ranzato <andrea@cynkra.com>
2023-09-23 19:09:45 +0200

    Forgotten from https://github.com/finddx/find.dxgap/pull/135

--- R/report.R
+++ R/report.R
@@ -5,8 +5,8 @@ render_report <- function(template_path = "inst/template/eda.Rmd",
   stopifnot(is.null(output_file) || is.character(output_file))
 
   lst_df <- load(.year = year)
-  dm_hbc <- build_dm(lst_df, hbc = TRUE)
-  dm_non_hbc <- build_dm(lst_df, hbc = FALSE)
+  dm_hbc <- build_dm(lst_df, is_hbc = TRUE)
+  dm_non_hbc <- build_dm(lst_df, is_hbc = FALSE)
   data_tbl <- build_tbl(dm_hbc, dm_non_hbc)
 
   # if output_file is NULL knit to temp file and open with Viewer/Rstudio browser
