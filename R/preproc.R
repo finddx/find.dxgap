d86f453dcc1b0761c1450445a008f38df90c321b
Andrea Ranzato <andrea@cynkra.com>
2023-09-23 11:28:02 +0200

    Smaller steps in recipe

--- R/preproc.R
+++ R/preproc.R
@@ -1,12 +1,41 @@
-get_core_recipe <- function(tbl) {
+get_core_recipe_pop_total <- function(tbl, neighbors) {
   recipes::recipe(formula = who_dx_gap ~ ., x = tbl) |>
+    recipes::step_filter_missing(recipes::all_predictors(), threshold = 0.9) |>
+    get_step_role() |>
+    get_core_recipe() |>
+    get_impute_with_pop_total_recipe(neighbors = neighbors) |>
+    get_finalize_recipe()
+}
+
+get_core_recipe_pop_density <- function(tbl, neighbors) {
+  recipes::recipe(formula = who_dx_gap ~ ., x = tbl) |>
+    recipes::step_filter_missing(recipes::all_predictors(), threshold = 0.9) |>
+    get_step_role() |>
+    get_core_recipe() |>
+    get_impute_with_pop_density_recipe(neighbors = neighbors) |>
+    get_finalize_recipe()
+}
+
+get_core_recipe <- function(recipe) {
+  recipe |>
+    recipes::step_filter(
+      !if_any(c("gdp", "e_inc_num", "who_dx_gap", "pop_total"), is.na)
+    ) |>
+    recipes::step_impute_median(recipes::has_role("impute_w_median")) |>
+    recipes::step_mutate(pop_100k = pop_total / 1e5) |>
+    recipes::step_rm(recipes::has_role("collinear"))
+}
+
+get_step_role <- function(recipe) {
+  recipe |>
     recipes::update_role(who_dx_gap, new_role = "outcome") |>
     recipes::update_role(country_code, new_role = "id") |>
-    recipes::step_filter_missing(recipes::all_predictors(), threshold = 0.9) |>
     recipes::update_role(
       dplyr::all_of(
-        c("c_cdr", "c_newinc_100k", "notified_ref_community", "notified_ref",
-          "e_mort_100k", "e_pop_num", "new_labconf")
+        c(
+          "c_cdr", "c_newinc_100k", "notified_ref_community", "notified_ref",
+          "e_mort_100k", "e_pop_num", "new_labconf"
+        )
       ),
       new_role = "collinear"
     ) |>
@@ -16,33 +45,53 @@ get_core_recipe <- function(tbl) {
     ) |>
     recipes::update_role(
       dplyr::all_of(
-        c("e_inc_100k", "e_inc_num", "c_newinc", "pop_total",
-          "pop_density", "pop_urban_perc", "gdp")
+        c(
+          "e_inc_100k", "e_inc_num", "c_newinc", "pop_total",
+          "pop_density", "pop_urban_perc", "gdp"
+        )
       ),
       new_role = "impute_w_median"
-    ) |>
-    recipes::step_filter(
-      !if_any(c("gdp", "e_inc_num", "who_dx_gap", "pop_total"), is.na)
-    ) |>
-    recipes::step_impute_median(recipes::has_role("impute_w_median")) |>
-    recipes::step_mutate(pop_100k = pop_total / 1e5) |>
-    recipes::step_rm(recipes::has_role("collinear")) |>
-    recipes::update_role(
-      dplyr::all_of(c("gdp", "e_inc_num", "pop_total")),
-      new_role = "imputer_knn",
-      old_role = "impute_w_median"
-    ) |>
-    recipes::step_impute_knn(
-      recipes::all_predictors(),
-      neighbors = 5,
-      impute_with = recipes::imp_vars(recipes::has_role("imputer_knn"))
-    ) |>
+    )
+}
+
+get_finalize_recipe <- function(recipe) {
+  recipe |>
     recipes::step_rm(e_inc_num, e_inc_100k, c_newinc) |>
     recipes::step_zv(recipes::all_numeric_predictors())
 }
 
-get_log_recipe <- function(core_recipe) {
-  core_recipe |>
+get_impute_with_pop_total_recipe <- function(recipe, neighbors) {
+  recipe |>
+    update_role_impute_knn(.vec = c("gdp", "e_inc_num", "pop_total")) |>
+    impute_knn(.neighbors = neighbors)
+}
+
+get_impute_with_pop_density_recipe <- function(recipe, neighbors) {
+  recipe |>
+    update_role_impute_knn(.vec = c("gdp", "e_inc_num", "pop_density")) |>
+    impute_knn(.neighbors = neighbors)
+}
+
+update_role_impute_knn <- function(recipe, .vec) {
+  recipe |>
+    recipes::update_role(
+      dplyr::all_of(.vec),
+      new_role = "imputer_knn",
+      old_role = "impute_w_median"
+    )
+}
+
+impute_knn <- function(recipe, .neighbors) {
+  recipe |>
+    recipes::step_impute_knn(
+      recipes::all_predictors(),
+      neighbors = .neighbors,
+      impute_with = recipes::imp_vars(recipes::has_role("imputer_knn"))
+    )
+}
+
+get_log_recipe <- function(recipe) {
+  recipe |>
     recipes::step_mutate_at(
       recipes::all_numeric_predictors() & -recipes::all_factor(),
       fn = ~ dplyr::if_else(.x == 0, 1, .x) # TODO: is this valid?
@@ -52,22 +101,22 @@ get_log_recipe <- function(core_recipe) {
     )
 }
 
-get_normalize_recipe <- function(core_recipe) {
-  core_recipe |>
+get_normalize_recipe <- function(recipe) {
+  recipe |>
     recipes::step_normalize(
       recipes::all_numeric_predictors() & -recipes::all_factor()
     )
 }
 
-get_is_hbc_recipe <- function(core_recipe) {
-  core_recipe |>
+get_is_hbc_recipe <- function(recipe) {
+  recipe |>
     recipes::step_filter(is_hbc == 1) |>
     recipes::step_rm(is_hbc) |>
     get_pca_recipe()
 }
 
-get_pop_100k_recipe <- function(core_recipe) {
-  core_recipe |>
+get_pop_100k_recipe <- function(recipe) {
+  recipe |>
     recipes::step_mutate_at(
       recipes::all_numeric_predictors() &
         -recipes::all_factor() &
@@ -77,8 +126,8 @@ get_pop_100k_recipe <- function(core_recipe) {
     recipes::step_rm(pop_100k)
 }
 
-get_pca_recipe <- function(core_recipe) {
-  core_recipe |>
+get_pca_recipe <- function(recipe) {
+  recipe |>
     get_normalize_recipe() |>
     recipes::step_pca(
       recipes::all_numeric_predictors() & -recipes::all_factor(),
