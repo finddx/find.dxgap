df47123f5af9be29d72e123c752965db15a21139
Andrea Ranzato <andrea@cynkra.com>
2023-09-23 17:13:13 +0200

    Clearer test

--- tests/testthat/test-dm.R
+++ tests/testthat/test-dm.R
@@ -1,7 +1,7 @@
 skip_if(Sys.getenv("DXGAP_DATADIR") == "")
 data_list <- load(.year = 2019)
 test_that("build_dm() `hbc` works", {
-  dm <- build_dm(data_list)
+  dm <- build_dm(data_list, is_hbc = FALSE)
   expect_snapshot(dm::glimpse(dm))
   expect_snapshot(dm::dm_examine_constraints(dm))
   expect_snapshot({
