4647cac2c2a0c46763f54f35f478c6ebc37a8add
Andrea Ranzato <andrea@cynkra.com>
2023-09-26 11:32:13 +0200

    Refactor

--- R/dm.R
+++ R/dm.R
@@ -13,9 +13,10 @@ build_tbl <- function(dm_hbc, dm_non_hbc) {
 }
 
 build_dm <- function(data_list, year = NULL, is_hbc = TRUE) {
-  # prune_lst <- drop_cols(data_list, c("country", "g_whoregion"))
+  non_parent <- setdiff(names(data_list), "hbc")
+  subset_lst <- drop_cols(data_list, non_parent,  c("country", "g_whoregion"))
   hbc_df <-
-    data_list$hbc |>
+    subset_lst$hbc |>
     select(country_code, year, country) |>
     mutate(is_hbc = 1)
 
@@ -24,33 +25,23 @@ build_dm <- function(data_list, year = NULL, is_hbc = TRUE) {
     mutate(is_hbc = 0)
 
   country_df <- bind_rows(hbc_df, non_hbc_df)
-  data_list$hbc <- NULL
-  data_list$country <- country_df
+  subset_lst$hbc <- NULL
+  subset_lst$country <- country_df
 
+  dm_no_rel <- dm::dm(!!!subset_lst)
+  dm_ts <- set_dm_rels(dm_no_rel)
 
-
-  dm_no_rel <- dm::dm(!!!data_list)
-  is_hbc2 <- is_hbc_dm(dm_no_rel)
-  if (is_hbc2) {
-    dm_rel <- set_dm_rels(dm_no_rel, hbc)
-  } else {
-    dm_rel <- set_dm_rels(dm_no_rel, non_hbc)
-  }
-  dm_ts <- set_dm_colors(dm_rel)
   if (is.null(year)) {
     return(dm_ts)
   }
-  if (is_hbc2) {
-    dm_year <- dm::dm_filter(dm_ts, hbc = (year == !!year))
-  } else {
-    dm_year <- dm::dm_filter(dm_ts, non_hbc = (year == !!year))
-  }
-  dm_year
+
+  dm::dm_filter(dm_ts, country = (year == !!year))
+
 }
 
-drop_cols <- function(data_list, cols_to_drop) {
+drop_cols <- function(data_list, at, cols_to_drop) {
   data_list |>
-    purrr::map(~ dplyr::select(.x, -tidyselect::any_of(cols_to_drop)))
+    purrr::map_at(.at = at, .f = ~ dplyr::select(.x, -tidyselect::any_of(cols_to_drop)))
 }
 
 get_non_hbc_country_code <- function(hbc_df) {
@@ -62,54 +53,11 @@ get_non_hbc_country_code <- function(hbc_df) {
     tidyr::crossing(year = 2000:2099)
 }
 
-assign_non_hbc_df <- function(non_hbc_list, non_hbc_df) {
-  non_hbc_list[[length(non_hbc_list) + 1]] <- non_hbc_df
-  rlang::names2(non_hbc_list)[length(non_hbc_list)] <- "non_hbc"
-  return(non_hbc_list)
-}
-
-subset_hbc <- function(data_list, .year, .is_hbc) {
-  hbc_df <- data_list$hbc
-  if (.is_hbc) {
-    country_lst <- filter_hbc_country(data_list)
-    return(country_lst)
-  }
-  non_hbc_df <- get_non_hbc_country_code(hbc_df, year = .year)
-  non_hbc_list <- filter_non_hbc_country(data_list, non_hbc_df)
-  assign_non_hbc_df(non_hbc_list, non_hbc_df)
-}
-
-filter_hbc_country <- function(data_list) {
-  hbc_df <- data_list$hbc
-  purrr::map(
-    data_list,
-    ~ dplyr::semi_join(.x, hbc_df, by = dplyr::join_by(country_code, year))
-  )
-}
-
-filter_non_hbc_country <- function(data_list, non_hbc_df) {
-  list_data <- purrr::map(
-    data_list,
-    ~ dplyr::semi_join(.x, non_hbc_df, by = dplyr::join_by(country_code, year))
-  )
-  list_data$hbc <- NULL
-  list_data
-}
-
-is_hbc_dm <- function(dm) {
-  tbl_names <- names(dm)
-  if ("hbc" %in% tbl_names) {
-    return(TRUE)
-  }
-  FALSE
-}
-
-set_dm_rels <- function(dm, parent_tbl) {
-  parent_tbl <- rlang::ensym(parent_tbl)
+set_dm_rels <- function(dm) {
   dm |>
-    set_dm_pk_parent(!!parent_tbl) |>
+    set_dm_pk_parent() |>
     set_dm_pk() |>
-    set_dm_fk(!!parent_tbl)
+    set_dm_fk()
 }
 
 set_dm_pk <- function(dm) {
@@ -128,24 +76,23 @@ set_dm_pk <- function(dm) {
 }
 
 set_dm_fk <- function(dm, parent_tbl) {
-  parent_tbl <- rlang::ensym(parent_tbl)
   dm |>
-    dm::dm_add_fk(!!parent_tbl, c(year, country_code), wb_tot_pop) |>
-    dm::dm_add_fk(!!parent_tbl, c(year, country_code), wb_urb_pop) |>
-    dm::dm_add_fk(!!parent_tbl, c(year, country_code), wb_density_pop) |>
-    dm::dm_add_fk(!!parent_tbl, c(year, country_code), wb_gdp) |>
-    dm::dm_add_fk(!!parent_tbl, c(year, country_code), who_notifications) |>
-    dm::dm_add_fk(!!parent_tbl, c(year, country_code), who_estimates) |>
-    dm::dm_add_fk(!!parent_tbl, c(year, country_code), who_budget) |>
-    dm::dm_add_fk(!!parent_tbl, c(year, country_code), who_community) |>
-    dm::dm_add_fk(!!parent_tbl, c(year, country_code), who_sites) |>
-    dm::dm_add_fk(!!parent_tbl, c(year, country_code), who_expenditures) |>
-    dm::dm_add_fk(!!parent_tbl, c(year, country_code), gf_procurement)
+    dm::dm_add_fk(country, c(year, country_code), wb_tot_pop) |>
+    dm::dm_add_fk(country, c(year, country_code), wb_urb_pop) |>
+    dm::dm_add_fk(country, c(year, country_code), wb_density_pop) |>
+    dm::dm_add_fk(country, c(year, country_code), wb_gdp) |>
+    dm::dm_add_fk(country, c(year, country_code), who_notifications) |>
+    dm::dm_add_fk(country, c(year, country_code), who_estimates) |>
+    dm::dm_add_fk(country, c(year, country_code), who_budget) |>
+    dm::dm_add_fk(country, c(year, country_code), who_community) |>
+    dm::dm_add_fk(country, c(year, country_code), who_sites) |>
+    dm::dm_add_fk(country, c(year, country_code), who_expenditures) |>
+    dm::dm_add_fk(country, c(year, country_code), gf_procurement)
 }
 
-set_dm_pk_parent <- function(dm, parent_tbl) {
-  parent_tbl <- rlang::ensym(parent_tbl)
-  dm::dm_add_pk(dm, !!parent_tbl, c(year, country_code), check = TRUE)
+set_dm_pk_parent <- function(dm) {
+  dm |>
+    dm::dm_add_pk(country, c(year, country_code), check = TRUE)
 }
 
 set_dm_colors <- function(dm) {
@@ -153,6 +100,6 @@ set_dm_colors <- function(dm) {
     dm::dm_set_colors(
       "#5986C4" = starts_with("who"),
       "#70AD47FF" = starts_with("wb"),
-      "#ED7D31FF" = contains("hbc")
+      "#ED7D31FF" = contains("country")
     )
 }
