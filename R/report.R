e36c96f81a992b7cc7b835ba9798ea003d3ff663
Andrea Ranzato <andrea@cynkra.com>
2023-09-26 17:15:32 +0200

    Tweak file_name render_report()

--- R/report.R
+++ R/report.R
@@ -1,8 +1,11 @@
-render_report <- function(template_path = "inst/template/eda.Rmd",
+render_report <- function(template_name,
                                  year = 2019,
-                                 output_file = NULL) {
+                                 output_file = NULL,
+                          template_dir = "inst/template/") {
   stopifnot(is.numeric(year))
   stopifnot(is.null(output_file) || is.character(output_file))
+
+  template_path <- compose_file_path(template_name, template_dir)
 
   lst_df <- load()
   dm <- build_dm(lst_df, year = year)
