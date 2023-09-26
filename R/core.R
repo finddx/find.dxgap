efc3014dd1a6d058b0c0f5904c8a1723e4a96392
Andrea Ranzato <andrea@cynkra.com>
2023-09-26 10:32:59 +0200

    Draft core

--- R/core.R
+++ R/core.R
@@ -1,3 +1,30 @@
+get_core <- function(list) {
+  c_newinc_df <-
+    list$who_notifications |>
+    get_with_data_countries(c_newinc)
+
+  e_inc_num_df <-
+    list$who_estimates |>
+    get_with_data_countries(e_inc_num)
+
+  in_common_dxgap <- inner_join(c_newinc_df, e_inc_num, join_by(country_code))
+
+  to_nest_df(df_lst) |>
+    mutate(in_common_acrs_yr = map(data, get_core_countries)) |>
+    mutate(in_common_dxgap = list(in_common_dxgap)) |>
+    mutate(
+      final = map2(
+        in_common_acrs_yr,
+        in_common_dxgap,
+        semi_join,
+        join_by(country_code)
+      )
+    ) |>
+    select(name, final) |>
+    unnest(final)
+}
+
+
 get_core_countries <- function(data) {
   years_unique <-
     data |>
