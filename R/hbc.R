d99ae7c91622f7c67d0121bc1d71c5e1fda1c2c3
Andrea Ranzato <andrea@cynkra.com>
2023-09-26 09:18:26 +0200

    Move

--- R/hbc.R
+++ R/hbc.R
@@ -74,23 +74,3 @@ grow_hbc <- function(data) {
     ) |>
     tidyr::unnest(hbc_data)
 }
-
-get_core_countries <- function(data) {
-  years_unique <-
-    data |>
-    dplyr::distinct(year) |>
-    dplyr::pull(year)
-
-  if (length(years_unique) <= 1) {
-    rlang::abort(
-      c("Cannot build `core high TB burden country` data across years.",
-      i = "Did you call `tidy_hbc(data, year = NULL)`?.")
-    )
-  }
-
-  data |>
-    dplyr::select(country_code, year) |>
-    dplyr::group_split(year) |>
-    purrr::reduce(dplyr::inner_join, by = dplyr::join_by(country_code)) |>
-    dplyr::select(-tidyselect::starts_with("year"))
-}
