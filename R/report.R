b9730b21b82b30010171edd0ae9fe943de9fd65e
Andrea Ranzato <andrea@cynkra.com>
2023-09-26 17:35:51 +0200

    Early return

--- R/report.R
+++ R/report.R
@@ -12,9 +12,9 @@ render_report <- function(template_name,
   data_tbl <- build_tbl(dm)
 
   # if output_file is NULL knit to temp file and open with Viewer/Rstudio browser
-  path <- if (is.null(output_file)) {
+  if (is.null(output_file)) {
     temp_file <- tempfile(pattern = "my_rmd", fileext = ".html")
-    rmarkdown::render(
+    path <- rmarkdown::render(
       input = template_path,
       output_file = temp_file,
       params = list(
@@ -25,19 +25,19 @@ render_report <- function(template_name,
       envir = new.env()
     )
     rstudioapi::viewer(path)
-  } else {
-    rmarkdown::render(
-      input = template_path,
-      output_file = basename(output_file),
-      output_dir = dirname(output_file),
-      output_format = "html_document",
-      params = list(
-        dm = dm,
-        data_tbl = data_tbl,
-        year = year
-      ),
-      envir = new.env()
-    )
+    return(invisible(path))
   }
+  path <- rmarkdown::render(
+    input = template_path,
+    output_file = basename(output_file),
+    output_dir = dirname(output_file),
+    output_format = "html_document",
+    params = list(
+      dm = dm,
+      data_tbl = data_tbl,
+      year = year
+    ),
+    envir = new.env()
+  )
   invisible(path)
 }
