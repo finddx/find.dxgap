c3713f76c9e14863e78c78667d33fa0f48255d72
Andrea Ranzato <andrea@cynkra.com>
2023-09-26 13:06:27 +0200

    Is given

--- R/core.R
+++ R/core.R
@@ -52,13 +52,11 @@ get_core_countries <- function(data) {
 
 
 get_with_data_countries <- function(data, col, year = NULL) {
-  if (!is.null(year)) {
-    data <- dplyr::filter(data, year >= !!year)
-  }
   data |>
     dplyr::mutate(is_given = !is.na({{ col }})) |>
     dplyr::select(country_code, year, is_given) |>
     dplyr::group_split(year, .keep = FALSE) |>
     purrr::reduce(dplyr::inner_join, by = dplyr::join_by(country_code, is_given)) |>
+    dplyr::filter(is_given) |>
     dplyr::select(country_code)
 }
