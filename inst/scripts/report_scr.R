1a3c53de8e2ceb92f429d2917aa1cafca1d23450
Andrea Ranzato <andrea@cynkra.com>
2023-10-04 14:48:54 +0200

    More examples

--- inst/scripts/report_scr.R
+++ inst/scripts/report_scr.R
@@ -5,13 +5,32 @@ set_data_dir()
 render_report(
   "missing.Rmd",
   .year = NULL,
-  interactive = FALSE,
+  interactive = TRUE,
   .vars = NULL
-  #.vars = dxgap_constants$tb_vars
 )
 
-render_report("eda.Rmd", .year = 2019, .vars = dxgap_constants$tb_vars)
+# is recognized as the subset of variables that should be used in the lm
+dxgap_constants$tb_vars
+
+render_report("eda.Rmd", .year = 2019, .vars = c(dxgap_constants$tb_vars, "new_labconf"))
 render_bulk("eda.Rmd", year = 2018:2021, vars = dxgap_constants$tb_vars)
 
+
 render_report("explain_lm.Rmd", .year = 2019, .vars = dxgap_constants$tb_vars)
-render_bulk("explain_lm.Rmd", year = 2018:2021, vars = dxgap_constants$tb_vars)
+render_bulk("explain_lm.Rmd", year = 2016:2019, vars = dxgap_constants$tb_vars)
+
+# or, alternatively remove xpert and render up to 2021 given that xpert is
+# given up to 2020 and 2021
+render_bulk(
+  "explain_lm.Rmd",
+  year = 2016:2021,
+  vars = setdiff(dxgap_constants$tb_vars, "xpert")
+)
+
+read_who("who_2023-08-30_laboratories.csv") |>
+  tidy_who() |>
+  filter(str_detect(variable, "m_wrd")) |>
+  filter(!is.na(value)) |>
+  pull(year) |>
+  range()
+
