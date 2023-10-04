875896cd2ce8a39c144a100470084945a813c774
Andrea Ranzato <andrea@cynkra.com>
2023-10-04 13:16:44 +0200

    Examples

--- inst/scripts/report_scr.R
+++ inst/scripts/report_scr.R
@@ -1,5 +1,17 @@
 pkgload::load_all()
 
+set_data_dir()
+
+render_report(
+  "missing.Rmd",
+  .year = NULL,
+  interactive = FALSE,
+  .vars = NULL
+  #.vars = dxgap_constants$tb_vars
+)
+
 render_report("eda.Rmd", .year = 2019, .vars = dxgap_constants$tb_vars)
+render_bulk("eda.Rmd", year = 2018:2021, vars = dxgap_constants$tb_vars)
+
 render_report("explain_lm.Rmd", .year = 2019, .vars = dxgap_constants$tb_vars)
-render_report("missing.Rmd", .year = NULL, .vars = dxgap_constants$tb_vars)
+render_bulk("explain_lm.Rmd", year = 2018:2021, vars = dxgap_constants$tb_vars)
