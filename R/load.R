6dcf33b5a1de1d21e109ddb534170050f225f39c
Andrea Ranzato <andrea@cynkra.com>
2023-11-09 00:26:16 +0100

    From disease tbl

--- R/load.R
+++ R/load.R
@@ -1,33 +1,19 @@
-#' @noRd
-#'
-#' @example
-#' list_dx()
-list_dx <- function(pattern = "csv", data_dir = Sys.getenv("DXGAP_DATADIR")) {
-  list.files(data_dir, pattern = pattern) |>
-    stringr::str_subset("masterlist", negate = TRUE)
+list_dxgap_datadir <- function(.pattern, .data_dir = Sys.getenv("DXGAP_DATADIR")) {
+  list.files(path = .data_dir, pattern = .pattern)
 }
 
-#' Load diagnostic gap data
-#'
-#' `load_dx()` loads all files frames created by [write_data_dir()] into a
-#' list of tibbles.
-#'
-#' @seealso [build_dm()]
-#'
-#' @return A list of tibbles.
-#'
-#' @export
-#'
-#' @examples \dontrun{
-#' load_dx()
-#' }
-load_dx <- function() {
-  load_dx_impl()
-}
-
-load_dx_impl <- function(data_files = list_dx()) {
-
-  lst_df <- import_bulk(lst_df, data_name = data_files)
+load_dx <- function(disease, data_dir = Sys.getenv("DXGAP_DATADIR")) {
+  disease_df <-
+    dxgap_diseases |>
+    dplyr::filter(disease == !!disease) |>
+    tidyr::unnest(tables) |>
+    dplyr::mutate(date_pattern = "\\d{4}-\\d{2}-\\d{2}") |>
+    dplyr::mutate(
+      pattern = stringr::str_c(provider_id, date_pattern, dataset_type, sep = "_")
+    )
+  regex_pattern <- paste(disease_df$pattern, collapse = "|")
+  data_dir_list <- list_dxgap_datadir(.data_dir = data_dir, .pattern = regex_pattern)
+  lst_df <- import_bulk(lst_df, data_name = data_dir_list)
 
   # HBC countries --------------------------------------------------------------
 
