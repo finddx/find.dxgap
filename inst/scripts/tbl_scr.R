fe7a9c2f7a47eb59ba7195b36008acf7f49ddba3
Andrea Ranzato <andrea@cynkra.com>
2023-11-08 22:16:13 +0100

    Ts example

--- inst/scripts/tbl_scr.R
+++ inst/scripts/tbl_scr.R
@@ -8,3 +8,10 @@ data_tbl <- build_tbl(dm)
 dm::dm_draw(dm)
 dm::dm_examine_constraints(dm)
 
+
+dm_ts <- build_dm(df_lst, year = NULL)
+data_tbl_ts <- build_tbl(dm_ts)
+
+dm::dm_draw(dm_ts)
+dm::dm_examine_constraints(dm_ts)
+
