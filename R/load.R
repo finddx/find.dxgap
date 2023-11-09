ea501703857906c197ec682e85766997d743de98
Andrea Ranzato <andrea@cynkra.com>
2023-11-09 22:39:50 +0100

    Use

--- R/load.R
+++ R/load.R
@@ -4,22 +4,14 @@ list_dxgap_datadir <- function(.pattern, .data_dir = Sys.getenv("DXGAP_DATADIR")
 
 load_dx <- function(disease, data_dir = Sys.getenv("DXGAP_DATADIR")) {
   check_supported_disease(disease)
-  disease_df <-
-    dxgap_diseases |>
-    dplyr::filter(disease == !!disease) |>
-    tidyr::unnest(tables) |>
-    dplyr::mutate(date_pattern = "\\d{4}-\\d{2}-\\d{2}") |>
-    dplyr::mutate(
-      pattern = stringr::str_c(provider_id, date_pattern, dataset_type, sep = "_")
-    )
+  disease_df <- generate_disease_pattern(dxgap_diseases, disease)
   regex_pattern <- paste(disease_df$pattern, collapse = "|")
-  data_dir_list <- list_dxgap_datadir(.data_dir = data_dir, .pattern = regex_pattern)
-  lst_df <- import_bulk(lst_df, data_name = data_dir_list)
+  data_files <- list_dxgap_datadir(.data_dir = data_dir, .pattern = regex_pattern)
+  lst_df <- import_bulk(lst_df, data_name = data_files)
 
   tbl_list <- switch(disease,
     tb = tb_load(lst_df)
   )
-
   tbl_list
 }
 
