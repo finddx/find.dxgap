cd0dcc27ae313a650d53ffba14d4037e661ad509
Andrea Ranzato <andrea@cynkra.com>
2023-11-16 14:57:41 +0100

    Docs

--- R/load.R
+++ R/load.R
@@ -2,6 +2,24 @@ list_dxgap_datadir <- function(.pattern, .data_dir = Sys.getenv("DXGAP_DATADIR")
   list.files(path = .data_dir, pattern = .pattern)
 }
 
+#' Load diagnostic gap data for given disease
+#'
+#' `load_dx()` loads all files for given disease into a
+#' list of tibbles.
+#'
+#' @param disease A character of lenght one identifying the disease for which
+#'   the user wants to build a wide table. The tibble `dxgap_diseases` shows the
+#'   diseases that are currently supported.
+#' @seealso [build_dm()]
+#' @seealso [build_tbl()]
+#'
+#' @return A list of tibbles.
+#'
+#' @export
+#'
+#' @examples \dontrun{
+#' load_dx("tb")
+#' }
 load_dx <- function(disease) {
   load_dx_impl(.disease = disease)
 }
@@ -11,6 +29,7 @@ load_dx_impl <- function(.disease, .dxgap_diseases = dxgap_diseases, data_dir = 
   pattern <- "\\d{4}-\\d{2}-\\d{2}"
   disease_df <- generate_disease_pattern(.dxgap_diseases, .disease, pattern)
   regex_pattern <- paste(disease_df$pattern, collapse = "|")
+  # TODO: listing files is not optimal. It might be as long as the data dir is kept clean.
   data_files <- list_dxgap_datadir(.data_dir = data_dir, .pattern = regex_pattern)
   check_clean_data_dir(data_files, .pattern = pattern)
   lst_df <- import_bulk(lst_df, data_name = data_files)
