ccb41b4b6e68f22ae5cf7067b7d79a7782f0241c
Andrea Ranzato <andrea@cynkra.com>
2023-11-23 15:11:25 +0100

    Finalize

--- R/mod_helpers.R
+++ R/mod_helpers.R
@@ -1,11 +1,17 @@
-#' Extract coefficients from final fit
+#' Helpers to work out `run_mod()`'s output
 #'
-#' `extract_mod_coeff()` is an helper functions to extract coefficient estimates
-#' from the best model returned by [run_mod()].
+#' These helpers extract elements of the list returned by [run_mod()].
 #'
-#' @param mod_out_list A list object as returned by [run_mod()].
+#' @name mod_helpers
+NULL
+
+#' Extract lm model fit object
+#'
+#' `extract_mod_rank()` extracts a tibble summarising the ranking of
+#' pre-processing steps.
+#'
 #' @rdname mod_helpers
-#' @return `extract_mod_coeff()` returns a tibble.
+#' @return `extract_mod_rank()` returns a tibble.
 #' @export
 #' @examples
 #' \dontrun{
@@ -17,6 +23,23 @@
 #'   metrics = tb_mod_const$metrics,
 #'   rank_metric = tb_mod_const$rank_metric,
 #' )
+#' extract_mod_rank(mod_objects)
+#' }
+extract_mod_rank <- function(mod_out_list) {
+  stopifnot(is.list(mod_out_list) && length(mod_out_list) == 3)
+  mod_out_list$rank
+}
+
+#' Extract coefficients from final fit
+#'
+#' `extract_mod_coeff()` extracts coefficient estimates from the best model.
+#'
+#' @param mod_out_list A list object as returned by [run_mod()].
+#' @rdname mod_helpers
+#' @return `extract_mod_coeff()` returns a tibble.
+#' @export
+#' @examples
+#' \dontrun{
 #' extract_mod_coeff(mod_objects)
 #' }
 extract_mod_coeff <- function(mod_out_list) {
@@ -27,8 +50,7 @@ extract_mod_coeff <- function(mod_out_list) {
 
 #' Extract lm model fit object
 #'
-#' `extract_mod_fit()` is an helper function to extract the model object from
-#' the best model returned by [run_mod()].
+#' `extract_mod_fit()` extracts the best model object.
 #'
 #' @rdname mod_helpers
 #' @return `extract_mod_fit()` returns an object of class `_lm`.
@@ -46,26 +68,4 @@ extract_mod_fit <- function(mod_out_list) {
   stopifnot(is.list(mod_out_list) && length(mod_out_list) == 3)
   mod_out_list$final_fit |>
     workflowsets::extract_fit_parsnip(id = pull_mod_best(mod_out_list$rank))
-}
-
-#' Extract lm model fit object
-#'
-#' `extract_mod_fit()` is an helper function to extract the model object from
-#' the best model returned by [run_mod()].
-#'
-#' @rdname mod_helpers
-#' @return `extract_mod_fit()` returns an object of class `_lm`.
-#' @export
-#' @examples
-#' \dontrun{
-#' lm_obj <- extract_mod_fit(mod_objects)
-#' plot_check <- performance::check_model(
-#'   lm_obj,
-#'   check = c("linearity", "normality", "qq", "outliers"),
-#'   theme = "ggplot2::theme_minimal"
-#' )
-#' }
-extract_mod_rank <- function(mod_out_list) {
-  stopifnot(is.list(mod_out_list) && length(mod_out_list) == 3)
-  mod_out_list$rank
 }
