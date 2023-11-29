52ae219f47fbb2c10e8068e926dbe79b6e933f0f
Andrea Ranzato <andrea@cynkra.com>
2023-11-29 15:21:11 +0100

    Rework tests

--- tests/testthat/test-who_expenditure.R
+++ tests/testthat/test-who_expenditure.R
@@ -1,22 +1,24 @@
-skip_if(Sys.getenv("DXGAP_DATADIR") == "")
-skip_if_offline()
-skip_if_not_available("https://extranet.who.int/tme/generateCSV.asp?ds=expenditure_utilisation")
-path <- download_who(url_endpoint = "expenditure_utilisation")
-on.exit(file.remove(path), add = TRUE)
-raw <- read_who(path)
-tidy <- tidy_who(raw)
 test_that("WHO expenditures data is downloaded correctly", {
+  skip_if_not_installed("curl")
+  skip_if_offline()
+  skip_if_not_available("https://extranet.who.int/tme/generateCSV.asp?ds=expenditure_utilisation")
+  path <- download_who(url_endpoint = "expenditure_utilisation")
+  on.exit(file.remove(path), add = TRUE)
   expect_true(file.exists(path))
 })
 
 test_that("WHO expenditures data is read and tidied correctly", {
+  skip_on_ci()
+  skip_if(Sys.getenv("DXGAP_DATADIR") == "")
+  path <- file.path(Sys.getenv("DXGAP_DATADIR"), "who_2023-07-28_expenditures.csv")
+  skip_if(!file.exists(path))
+  raw <- read_who(path)
+  tidy <- tidy_who(raw)
   expect_snapshot({
     constructive::construct(vctrs::vec_ptype(raw))
     constructive::construct(vctrs::vec_ptype(tidy))
   })
-})
-
-test_that("`country_code` entries are not missing", {
+  # `country_code` entries are not missing"
   expect_snapshot(
     tidy |>
       dplyr::distinct(country_code, country) |>
