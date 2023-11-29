259d71fbbfa67a772ce316eb7a845aa6b945c97d
Andrea Ranzato <andrea@cynkra.com>
2023-11-29 17:57:08 +0100

    Escape non ASCII to avoid getting R CMD warnings

--- R/country.R
+++ R/country.R
@@ -7,15 +7,16 @@ country_exclude_df <- tibble::tibble(
     "HMD", "JEY", "MTQ", "MYT", "NFK", "PCN", "REU", "SGS", "BLM", "SHN", "SPM",
     "SJM", "TWN", "UMI", "VAT", "ESH", "ALA"
   ),
+  # we escape non ASCII to avoid getting R CMD warnings
   country = c(
     "Antarctica", "Bouvet Island", "British Indian Ocean Territory",
     "Caribbean Netherlands", "Christmas Island", "Cocos (Keeling) Islands",
     "Falkland Islands", "French Guiana", "French Southern Territories",
     "Guadeloupe", "Guernsey", "Heard & McDonald Islands", "Jersey", "Martinique",
-    "Mayotte", "Norfolk Island", "Pitcairn Islands", "Réunion",
-    "South Georgia & South Sandwich Islands", "St. Barthélemy", "St. Helena",
+    "Mayotte", "Norfolk Island", "Pitcairn Islands", "R\\u00e9union",
+    "South Georgia & South Sandwich Islands", "St. Barth\\u00e9lemy", "St. Helena",
     "St. Pierre & Miquelon", "Svalbard & Jan Mayen", "Taiwan",
     "United States Minor Outlying Islands (the)", "Vatican City",
-    "Western Sahara", "Åland Islands"
+    "Western Sahara", "\\u00c5land Islands"
   ),
 )
