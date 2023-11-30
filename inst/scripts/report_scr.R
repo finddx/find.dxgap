9a781e74af88d8a7a63f3bf596cc17b76fb0435d
Andrea Ranzato <andrea@cynkra.com>
2023-11-30 12:59:43 +0100

    Tweak report scr

--- inst/scripts/report_scr.R
+++ inst/scripts/report_scr.R
@@ -6,15 +6,15 @@ render_report(
   "missing.Rmd",
   disease = "tb",
   year = NULL,
-  interactive = TRUE,
+  interactive = FALSE,
   vars = NULL
 )
 
 # is recognized as the subset of variables that should be used in the lm
 dxgap_const$tb_vars
 
-render_report("eda.Rmd", disease = "tb", year = 2019, vars = c(dxgap_const$tb_vars, "new_labconf"))
-render_bulk("eda.Rmd", disease = "tb", year = 2018:2021, vars = dxgap_const$tb_vars)
+render_report("eda.Rmd", disease = "tb", year = 2019, vars = dxgap_const$tb_vars)
+render_bulk("eda.Rmd", disease = "tb", year = 2016:2021, vars = dxgap_const$tb_vars)
 
 render_report("explain_lm.Rmd", disease = "tb", year = 2019, vars = dxgap_const$tb_vars)
 render_bulk("explain_lm.Rmd", disease = "tb", years = 2016:2021, vars = dxgap_const$tb_vars)
@@ -26,4 +26,4 @@ read_who("who_2023-08-30_laboratories.csv") |>
   pull(year) |>
   range()
 
-render_report("eda_ts.Rmd", disease = "tb", vars = dxgap_const$tb_vars)
+render_report("eda_ts.Rmd", disease = "tb", vars = dxgap_const$tb_vars, interactive = FALSE)
