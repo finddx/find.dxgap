6cd6d31fa272638687b54fcc369d58f7387f2e6a
Andrea Ranzato <andrea@cynkra.com>
2023-12-01 00:58:18 +0100

    Tweak script

--- inst/scripts/import_script.R
+++ inst/scripts/import_script.R
@@ -1,29 +1,32 @@
 pkgload::load_all()
 
-lst_df <- import_bulk(lst_df, c("gf_2023-07-26_procurement.csv",
-                                 "wb_2023-07-28_pop_urban.csv",
-                                 "wb_2023-08-31_pop_density.csv",
-                                 "wb_2023-09-07_gdp.csv",
-                                 "wb_2023-08-31_pop_total.csv",
-                                 "who_2023-07-28_budget.csv",
-                                 "who_2023-07-28_community.csv",
-                                 "who_2023-07-28_estimates.csv",
-                                 "who_2023-07-28_expenditures.csv",
-                                 "who_2023-07-28_notifications.csv",
-                                 "who_2023-08-30_laboratories.csv",
-                                 "who_2023-07-28_hbc.csv"), 2019)
+lst_df <- import_bulk(
+  c(
+    "gf_2023-07-26_procurement.csv",
+    "wb_2023-07-28_pop_urban.csv",
+    "wb_2023-08-31_pop_total.csv",
+    "wb_2023-09-07_gdp.csv",
+    "wb_2023-10-04_pop_density.csv",
+    "who_2023-07-28_budget.csv",
+    "who_2023-07-28_community.csv",
+    "who_2023-07-28_estimates.csv",
+    "who_2023-07-28_expenditures.csv",
+    "who_2023-07-28_hbc.csv",
+    "who_2023-08-30_laboratories.csv",
+    "who_2023-11-28_notifications.csv"
+  )
+)
 
-lst_df <- import_tbl(lst_df, "gf_2023-07-26_procurement.csv", 2019)
-lst_df <- import_tbl(lst_df, "wb_2023-07-28_pop_urban.csv", 2019)
-lst_df <- import_tbl(lst_df, "wb_2023-08-31_pop_density.csv", 2019)
-lst_df <- import_tbl(lst_df, "wb_2023-08-31_pop_total.csv", 2019)
-lst_df <- import_tbl(lst_df, "wb_2023-09-07_gdp.csv", 2019)
-lst_df <- import_tbl(lst_df, "who_2023-07-28_budget.csv", 2019)
-lst_df <- import_tbl(lst_df, "who_2023-07-28_community.csv", 2019)
-lst_df <- import_tbl(lst_df, "who_2023-07-28_estimates.csv", 2019)
-lst_df <- import_tbl(lst_df, "who_2023-07-28_expenditures.csv", 2019)
-lst_df <- import_tbl(lst_df, "who_2023-07-28_notifications.csv", 2019)
-lst_df <- import_tbl(lst_df, "who_2023-08-30_laboratories.csv", 2019)
-lst_df <- import_tbl(lst_df, "who_2023-07-28_hbc.csv", 2019)
+tbl <- import_tbl("gf_2023-07-26_procurement.csv")
+tbl <- import_tbl("wb_2023-07-28_pop_urban.csv")
+tbl <- import_tbl("wb_2023-08-31_pop_total.csv")
+tbl <- import_tbl("wb_2023-09-07_gdp.csv")
+tbl <- import_tbl("wb_2023-10-04_pop_density.csv")
+tbl <- import_tbl("who_2023-07-28_budget.csv")
+tbl <- import_tbl("who_2023-07-28_community.csv")
+tbl <- import_tbl("who_2023-07-28_estimates.csv")
+tbl <- import_tbl("who_2023-07-28_expenditures.csv")
+tbl <- import_tbl("who_2023-07-28_hbc.csv")
+tbl <- import_tbl("who_2023-08-30_laboratories.csv")
+tbl <- import_tbl("who_2023-11-28_notifications.csv")
 
-lst_df
