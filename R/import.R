4d56a2596e816a6270c1aa0c8a1f5d13ac9ec430
Andrea Ranzato <andrea@cynkra.com>
2023-12-06 13:07:12 +0100

    Use impl

--- R/import.R
+++ R/import.R
@@ -8,7 +8,7 @@
 import_tbls <- function(data_names) {
   stopifnot(is.character(data_names) && length(data_names) >= 1)
   names(data_names) <- extract_name(data_names)
-  lst_df <- purrr::map(data_names, ~ import_tbl(.file_name = .x))
+  lst_df <- purrr::map(data_names, ~ import_tbl_impl(.file_name = .x))
   lst_df
 }
 
