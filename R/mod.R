f143a08336cbafc97477ee533d108187a2fa27bd
Andrea Ranzato <andrea@cynkra.com>
2023-12-06 10:44:38 +0100

    Match with recipe

--- R/mod.R
+++ R/mod.R
@@ -30,7 +30,7 @@
 #'   .tbl = tbl,
 #'   .neighbors = 5,
 #'   .threshold = 0.25,
-#'   .impute_vars = c("gdp", "e_inc_num", "pop_total")
+#'   .impute_with = c("gdp", "e_inc_num", "pop_total")
 #'  )
 #'
 #' mod_list <- get_mod_mod("regression", "lm")
@@ -164,16 +164,16 @@ split_mod <- function(tbl, .v) {
 #'   .tbl = tbl,
 #'   .neighbors = 5,
 #'   .threshold = 0.25,
-#'   .impute_vars = c("gdp", "e_inc_num", "pop_total")
+#'   .impute_with = c("gdp", "e_inc_num", "pop_total")
 #'  )
 #' }
-get_mod_preproc <- function(.tbl, .neighbors, .threshold, .impute_vars) {
+get_mod_preproc <- function(.tbl, .neighbors, .threshold, .impute_with) {
   tibble::lst(
     simple = get_recipe_tb(
       tbl = .tbl,
       neighbors = .neighbors,
       threshold = .threshold,
-      impute_vars = .impute_vars
+      impute_vars = .impute_with
     ),
     log_pop_total = get_log_recipe_tb(simple),
     norm_pop_total = get_normalize_recipe_tb(simple),
