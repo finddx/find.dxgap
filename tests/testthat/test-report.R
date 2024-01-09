ad7697ceb0ca5c9a17a8a00ad18816451e28002f
Andrea Ranzato <andrea@cynkra.com>
2024-01-09 17:33:23 +0100

    Tweak test

--- tests/testthat/test-report.R
+++ tests/testthat/test-report.R
@@ -3,10 +3,9 @@ test_that("render_report() works", {
   skip_if(Sys.getenv("DXGAP_DATADIR") == "")
   path <- render_report(
     "test.Rmd",
-    .disease = "tb",
-    .year = 2019,
-    interactive = FALSE,
-    template_dir = here::here("inst/template")
+    disease = "tb",
+    year = 2019,
+    interactive = FALSE
   )
   expect_true(file.exists(path))
   on.exit(unlink(path))
