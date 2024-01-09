0246f02652b6eac4af2c131e1e403d7b36db3b45
Andrea Ranzato <andrea@cynkra.com>
2024-01-09 14:21:33 +0100

    test-build_tbl

---
+++ tests/testthat/test-build_tbl.R
@@ -0,0 +1,25 @@
+test_that("build_tbl() works with non-NULL args.", {
+  skip_on_ci()
+  skip_if(Sys.getenv("DXGAP_DATADIR") == "")
+  tbl <- build_tbl(
+    "tb",
+    year = 2019,
+    estimated = "who_estimates.e_inc_num",
+    notified = "who_notifications.c_newinc",
+    vars = c("year", "country", "pop_density", "e_inc_num", "c_newinc")
+  )
+  expect_s3_class(tbl, "data.frame")
+})
+
+test_that("build_tbl() works with `NULL` args.", {
+  skip_on_ci()
+  skip_if(Sys.getenv("DXGAP_DATADIR") == "")
+  tbl <- build_tbl(
+    "tb",
+    year = 2019,
+    estimated = NULL,
+    notified = NULL,
+    vars = c("year", "country", "pop_density", "e_inc_num", "c_newinc")
+  )
+  expect_s3_class(tbl, "data.frame")
+})
