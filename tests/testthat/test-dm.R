a78fda9bd90e2d61a7f17616f5ebecf7efff3c4d
Andrea Ranzato <andrea@cynkra.com>
2024-01-09 12:25:10 +0100

    More tests

--- tests/testthat/test-dm.R
+++ tests/testthat/test-dm.R
@@ -39,6 +39,47 @@ test_that("build_dm() works and returns a time series", {
   })
 })
 
+test_that("build_dm() with `NULL` args.", {
+  skip_on_ci()
+  skip_if(Sys.getenv("DXGAP_DATADIR") == "")
+  data_list <- load_dx("tb")
+  dm <- build_dm(
+    data_list,
+    year = 2019,
+    estimated = NULL,
+    notified = NULL
+  )
+  expect_snapshot(dm::glimpse(dm))
+  expect_snapshot(dm::dm_examine_constraints(dm))
+  expect_snapshot({
+    dm::dm_examine_constraints(dm) |>
+      tibble::as_tibble() |>
+      dplyr::filter(problem != "") |>
+      dplyr::pull(problem) |>
+      writeLines()
+  })
+})
+
+test_that("build_dm() works and returns a time series with `NULL` args.", {
+  skip_on_ci()
+  skip_if(Sys.getenv("DXGAP_DATADIR") == "")
+  data_list <- load_dx("tb")
+  dm <- build_dm(
+    data_list,
+    estimated = NULL,
+    notified = NULL
+  )
+  expect_snapshot(dm::glimpse(dm))
+  expect_snapshot(dm::dm_examine_constraints(dm))
+  expect_snapshot({
+    dm::dm_examine_constraints(dm) |>
+      tibble::as_tibble() |>
+      dplyr::filter(problem != "") |>
+      dplyr::pull(problem) |>
+      writeLines()
+  })
+})
+
 test_that("build_tbl_impl() works", {
   skip_on_ci()
   skip_if(Sys.getenv("DXGAP_DATADIR") == "")
